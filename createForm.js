8bc5e97a2222e93b2cbbfaa7e811ab67ee9184a0
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-08-16 09:34:23 -0400

    reconfig to use for staff intake

--- createForm.js
+++ createForm.js
@@ -1,3 +1,5 @@
+const { stat } = require("fs");
+
 let id = ''
 const params = new URLSearchParams(window.location.search)
 for (const [key, value] of params) { id = value; }
@@ -44,11 +46,12 @@ async function populatePage(data) {
     document.getElementById('hrsOfService').innerHTML = data.hrsOfServices;
     document.getElementById('availability').innerHTML = data.availability;
     document.getElementById('notes').innerHTML = data.notes;
-    
-    //more to come as we get more data
+    sessionStorage.setItem('clientName', data.clientName)
     showPage()
-    const message = `A new client intake form was completed for ${data.clientName}`
-    notify('admin', message, 'general')
+    data = await appendStaffAssessment(data);
+    sessionStorage.setItem('staffName', data.staff)
+    submitForm(data, 'newIntakeFormStaff') 
+    updateClient(data)
 }
 
 //Turns off animation and shows the page with data fields completed
@@ -67,17 +70,12 @@ function showErrorMsg(err) {
     document.getElementById('errorMessageSection').style.display = "block"
 }
 
-const printToPDF = document.getElementById('printToPDF')
-printToPDF.addEventListener('click', (e) => {
-    window.print()
-    
-})
-
-function notify(to, message, type) {
+function notify(to, message, type, priority) {
     toSend = {
         'name': to,
         'notice': message,
-        'type': type
+        'type': type,
+        'priority': priority
     }
 
     const url = `https://pffm.azurewebsites.net/notices`
@@ -94,4 +92,89 @@ function notify(to, message, type) {
         .then(() => console.log(`message to ${to} was sent`))
         .catch(console.error)
     
+}
+
+async function appendStaffAssessment(data) {
+    document.getElementById('submit').addEventListener('click', (event) => {
+        data.staff = ''
+        data.assessDate = ''
+        data.readingLevel = ''
+        data.writingLevel = ''
+        data.ogl = ''
+        data.bgl = ''
+        data.mathLevel = ''
+        data.staff = document.getElementById('staff').value;
+        data.assessDate = document.getElementById('assessDate').value;
+        data.readingLevel = document.getElementById('readingLevel').value;
+        data.writingLevel = document.getElementById('writingLevel').value;
+        data.ogl = document.getElementById('ogl').value;
+        data.bgl = document.getElementById('bgl').value;
+        data.mathLevel = document.getElementById('mathLevel').value
+        return data
+    } )
+}
+
+async function submitForm(data, form) {
+  const document = {
+    'form': form,
+    'data': data
+  }
+  console.log(document)
+  fetch('https://pffm.azurewebsites.net/form', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin" : "*"
+    },
+    body: JSON.stringify(document)
+  })
+    .then(response => response.json())
+    .then(data => respond(data)) 
+    .catch((err) => showError(err))
+}
+
+function respond(data) {
+  let id = data.key
+  if (id) {
+    showSuccess(id) 
+  } else {
+    showError(data.error)
+  }
+}
+
+function showSuccess(id) {
+  document.getElementById('returnMessage').innerHTML = 'Form has been successfully submitted & notifications have been sent'
+  href = `https://phoenix-freedom-foundation-backend.webflow.io/completed-forms/new-client-intake-form-staff?id=${id}`
+    message = `You have an updated <a href=${href}>New Client Intake Form</a> to view`
+    let staff = sessionStorage.getItem('staffName')
+    let client = sessionStorage.getItem('clientName')
+    notify(staff, message, 'individual', 'not urgent')
+    notify(client, message, 'individual', 'not urgent')
+    notify('admin', message, 'individual', 'not urgent')
+    
+}
+
+
+function showError(err) {
+    console.error
+    document.getElementById('returnMessage').innerHTML = `An error occurred when submitting this form, which was ${err}. Please contact the administrator for help.`
+}
+
+
+async function updateClient(clientData) {
+    console.log(clientData)
+    const document = {
+        data: clientData,
+        clientName: clientData.clientName
+    }
+    fetch('https://pffm.azurewebsites.net/updateClient', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(document)
+    })
+        .then(() => console.log('resolved'))
+        .catch(console.error)
 }
